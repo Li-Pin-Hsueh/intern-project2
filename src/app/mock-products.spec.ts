20bafd9c61f294a21ad0ef0595665b57f1f58b1d
Vincent <pinxue.career@gmail.com>
2021-01-25 22:06:48 +0800

    create mock data

---
+++ src/app/mock-products.spec.ts
@@ -0,0 +1,7 @@
+import { Products } from './mock-products';
+
+describe('Products', () => {
+  it('should create an instance', () => {
+    expect(new Products()).toBeTruthy();
+  });
+});
--- src/app/product.ts
+++ src/app/product.ts
@@ -4,4 +4,5 @@ export interface Product {
     price: number ;
     inventory: number ;
     imgUrl: string ; 
+    type: string[] ;
 }
